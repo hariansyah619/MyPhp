e1b4e015e9dc24c5e7e61d5105f2f9e397e3d95f
ikhwan sulestra <ikhwansulestra12@gmail.com>
2018-08-26 18:45:36 +0700

    tambah
    ubah judul

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>UNIVERSITAS PUTERA BATAM - SISTEM INFORMASI AKADEMIK</title>
+        <title>FUTURE SCHOOL - SISTEM INFORMASI AKADEMIK</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="keywords" content="Universitas, Putera Batam, Universitas Putera Batam, Kuliah, Batam, Perkuliahan, Universitas Batam, University Batam, University, SIA, Sistem Informasi, Sistem Informasi Akademik, Sistem Informasi Mahasiswa" />
         <link href="images/icoupb.png" rel="shortcut icon" />
