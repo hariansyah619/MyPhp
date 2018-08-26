2a5e0c6dedb1be62528c2eec0183875f0e656374
kalosox619 <hariansyahkalosox@gmail.com>
2018-08-26 17:53:22 +0700

    original file

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UNIVERSITAS PUTERA BATAM - SISTEM INFORMASI AKADEMIK</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="keywords" content="Universitas, Putera Batam, Universitas Putera Batam, Kuliah, Batam, Perkuliahan, Universitas Batam, University Batam, University, SIA, Sistem Informasi, Sistem Informasi Akademik, Sistem Informasi Mahasiswa" />
+        <link href="images/icoupb.png" rel="shortcut icon" />
+        <link href="css/stylesiaportal.css" rel="stylesheet" type="text/css">
+    
+	<style type="text/css">
+    img {
+        width: 70px;
+		margin: 20px;
+    }
+    </style>
+	
+	</head>
+
+    <body>
+        <!-- pembungkusan Wrap-->
+        <div id="warp">
+            <!-- Permulaian Header-->        
+            <div id="header">            
+                <div class="wel">Welcome To</div>
+                <div class="upb">UNIVERSITAS PUTERA BATAM</div>
+            </div>
+            <!-- Akhir dari Header-->
+
+            <!-- Layout Main Menu-->
+            <div id="main_menu_sia">
+
+                <div class="sia">SISTEM INFORMASI AKADEMIK</div>	
+
+                <div class="clearfix">                  
+                    <div class="link_icon">
+                        <a href="http://www.upbatam.ac.id/" class="zoom">
+                            <img src="images/icon_website.jpg" alt="Website Future School" />
+                            <div class="title clearfix">
+                                WEBSITE
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="link_icon">
+                        <a href="http://sia.upbatam.ac.id/siam/" class="zoom">
+                            <img src="images/icon_murid.png" alt="sistim informasi murid future school" />
+                            <div class="title clearfix">
+                                MURID
+                            </div>
+                        </a>
+                    </div>
+
+                        <div class="link_icon">
+                            <a href="http://sia.upbatam.ac.id/siad/" class="zoom">
+                                <img src="images/icon_guru.jpg" alt="Sistem Informasi Guru Future School" />
+                                <div class="title clearfix">
+                                    GURU
+                                </div>
+                            </a>
+                        </div>                        
+
+					<div class="link_icon">
+                        <a href="http://forum.upbatam.ac.id/" class="putar">
+                            <img src="images/icon_forum.png" alt="Forum Future School" width="100" />
+                            <div class="title clearfix">
+                                FORUM
+                            </div>
+                        </a>
+                    </div>
+					
+					<div class="link_icon">
+                        <a href="http://forum.upbatam.ac.id/" class="putar">
+                            <img src="images/icon_staff.jpg" alt="Sistem Informasi Staff Future School" width="100" />
+                            <div class="title clearfix">
+                                STAFF
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <!-- Akhir dari main_menu-->
+
+            <!-- Awal dari Kolom Keterangan-->
+            <div id="kolom_keterangan" class="clearfix">
+                <!-- Awal dari kolom Visi dan Misi-->
+                <div id="kolom_visi">
+                    <strong class="judul">VISI</strong>
+                    <br class="clearfix" />
+                    <br class="clearfix" />
+                    Menjadi Universitas terkemuka berbasis teknologi informasi dan komunikasi yang menghasilkan sumber daya manusia yang berkompeten di bidangnya.
+                    <br class="clearfix" />
+                    <br class="clearfix" />
+                    <strong class="judul">MISI</strong>
+                    <br class="clearfix" />
+                    <ol>
+                        <li>Menyelenggarakan pendidikan yang bermutu tinggi berbasis teknologi informasi dan komunikasi untuk menciptakan sumber daya manusia yang berkompeten di bidangnya.</li>
+                        <li>Menyelenggarakan penelitian terapan di berbagai bidang ilmu pengetahuan dan teknologi.</li>
+                        <li>Menyelenggarakan pengabdian kepada masyarakat melalui pemanfaatan ilmu pengetahuan dan teknologi.</li>
+                        <li>Menerapkan manajemen pendidikan tinggi yang efektif dan efisien dan mengembangkan jaringan kerjasama industri dan kemitraan yang berkelanjutan sebagai respon atas perubahan arus dan daya saing global.</li>
+                    </ol>
+                </div>
+                <!-- Akhir dari kolom Visi dan Misi-->
+
+                <!-- Awal dari kolom alamat-->
+                <div id="kolom_alamat">
+                    <strong class="judul">KAMPUS PUTERA BATAM</strong>
+                    <div class="clearfix">
+                        <div class="alamat">
+                            <div class="banner">TEMBESI</div>
+                            Jalan R. Soeprapto<br />
+                            Muka Kuning - Batam<br /><br />
+                        </div>
+
+                        <div class="alamat">
+                            <div class="banner">NAGOYA</div>
+                            Jalan Raden Patah<br />
+                            Nagoya - Batam<br /><br />
+                        </div>
+
+                        <div class="alamat">
+                            <div class="banner">TIBAN</div>
+                            Jalan Gajah Mada<br />
+                            Komplek Cipta Puri<br />
+                            Tiban - Batam<br />
+                        </div>
+                    </div>
+                </div>            	
+                <!-- Akhir dari Kolom alamat-->
+            </div>
+            <!-- Akhir dari kolom keterangan-->
+
+            <!-- Awal dari Footer-->
+            <div id="footer" class="clearfix">
+                &copy; 2018. Universitas Putera Batam
+                <br />
+                info@upbatam.ac.id
+            </div>
+            <!-- Akhir dari footer-->
+        </div>
+        <!-- Akhir dari Pembungkusan Wrap-->
+    </body>
+</html>
